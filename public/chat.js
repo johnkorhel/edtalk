14b9126cd2e2142b642a18a9d48b5a741149a2ef
johnkorhel <korheljp@s.dcsdk12.org>
2019-01-02 13:47:21 -0700

    updated event listeners and made UI more user friendly

--- public/chat.js
+++ public/chat.js
@@ -6,17 +6,21 @@ var message = document.getElementById('message'),
   btn = document.getElementById('send'),
   output = document.getElementById('output'),
   username = document.getElementById('username'),
-  userBtn = document.getElementById('usernameButton');
+  userBtn = document.getElementById('usernameButton'),
+  titleBar = document.getElementById('chatUserTitle');
 
-
+//Listen for click on username prompt and populate username value
 userBtn.addEventListener('click', function() {
   while (document.getElementById("username").value == "") {
     return false;
   };
   username = document.getElementById("username").value;
   $('#usernameModal').modal('hide');
+  titleBar.innerHTML += '<h2 class="animated fadeIn chatTitle">' + document.getElementById("username").value + '| Socratic Seminar</h2>';
+  output.innerHTML += '<h1 class="textFaded">' + document.getElementById("username").value + ', welcome to the discussion!</h1><hr class="animated fadeIn"></hr>';
 });
 
+//Listen for enter keypress on username prompt and populate username value
 username.addEventListener('keypress', function(e) {
   while (document.getElementById("username").value == "") {
     return false;
@@ -26,10 +30,12 @@ username.addEventListener('keypress', function(e) {
     event.preventDefault();
     username = document.getElementById("username").value;
     $('#usernameModal').modal('hide');
+    titleBar.innerHTML += '<h2 class="animated fadeIn chatTitle">' + document.getElementById("username").value + '| Socratic Seminar</h2>';
+    output.innerHTML += '<h1 class="animated fadeIn textFaded">' + document.getElementById("username").value + ', welcome to the discussion!</h1><hr class="animated fadeIn"></hr>';
   }
 });
 
-
+//Listen for click on message prompt and emit chat value to other sockets
 btn.addEventListener('click', function() {
   while (document.getElementById("message").value == "") {
     return false;
@@ -41,6 +47,7 @@ btn.addEventListener('click', function() {
   document.getElementById('message').value = '';
 });
 
+//Listen for enter key on message prompt and emit chat value to other sockets
 message.addEventListener('keypress', function(e) {
   while (document.getElementById("message").value == "") {
     return false;
@@ -55,9 +62,10 @@ message.addEventListener('keypress', function(e) {
     document.getElementById('message').value = '';
   }
 });
+
 // Listen for events
 socket.on('chat', function(data) {
   output.innerHTML +=
     '<h2 class="animated fadeIn userText" id="userText"> <span id="user">' +
-    data.username + '</span>: ' + data.message + '</h2>' + '<hr></hr>';
+    data.username + '</span>: ' + data.message + '</h2>' + '<hr class="animated fadeIn"></hr>';
 });
