3cdcbe76aa2a1bc0cb06bfe7bc524c41eafceecc
CamGenius <CamGenius@users.noreply.github.com>
2019-01-16 12:15:41 -0700

    Fixed stuff

--- edtalk/Hubs/ChatHub.cs
+++ edtalk/Hubs/ChatHub.cs
@@ -5,9 +5,37 @@ namespace edtalk.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        //****************************************************
+        public async Task Connect(string username, string room)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("NewUser", $"{username} has joined the group chat");
         }
+        public async Task Disconnect(string username, string room)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("GoneUser", $"{username} has left the group chat");
+        }
+        //****************************************************
+
+
+        //****************************************************
+        public async Task Active(string username, string room)
+        {
+            await Clients.Group(room).SendAsync("ActiveUser", $"{username} has become active");
+        }
+        public async Task Inactive(string username, string room)
+        {
+            await Clients.Group(room).SendAsync("InactiveUser", $"{username} has become inactive");
+        }
+        //****************************************************
+
+
+        //****************************************************
+        public async Task SendMessage(string username, string message, string room)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", username, message);
+        }
+        //****************************************************
     }
 }
