e40a5cecba82690ca867e1a2b7e83bc6bbb3f51f
CamGenius <CamGenius@users.noreply.github.com>
2019-01-17 10:46:49 -0700

    Added HTML

--- edtalk/wwwroot/js/chat.js
+++ edtalk/wwwroot/js/chat.js
@@ -14,7 +14,7 @@ connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
+document.getElementById("sendMessageButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
