37773179cd3de7288b0593515bd0cf649b0e76a3
CamGenius <CamGenius@users.noreply.github.com>
2019-01-25 21:11:49 -0700

    Fully working rooms

--- edtalk/wwwroot/js/chat.js
+++ edtalk/wwwroot/js/chat.js
@@ -10,14 +10,31 @@ connection.on("ReceiveMessage", function (user, message) {
     document.getElementById("messagesList").appendChild(li);
 });
 
+connection.on("NewUser", function (user) {
+    var encodedMsg = user + " Has Joined the Chat Room";
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
 document.getElementById("sendMessageButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
+    var user = document.getElementById("usernameInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    var room = document.getElementById("roomInput").value;
+    connection.invoke("SendMessage", user, message, room).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("loginButton").addEventListener("click", function (event) {
+    var user = document.getElementById("usernameInput").value;
+    var room = document.getElementById("roomInput").value;
+    connection.invoke("Connect", user, room).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
